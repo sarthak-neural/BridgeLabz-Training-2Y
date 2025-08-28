2495fc0568acd81a73d0646a2bae2637f876cff5
sarthak-neural <sarthakagarwal027@gmail>
2025-08-28 11:56:50 +0530

    [Sarthak]-added practice problems

---
+++ Programming_Element_Practice/VolumeOfCylinder.java
@@ -0,0 +1,8 @@
+public class VolumeOfCylinder {
+    public static void main(String[] args) {
+        double radius = 3;
+        double height = 10;
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("Volume of cylinder = " + volume);
+    }
+}
