a1f1c057fdf909a25990c5be6072e10bdb0f3dff
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:27:45 +0530

    Delete Programming_Element_Practice directory

--- Programming_Element_Practice/CelsiusToFahrenheit.java
+++
@@ -1,7 +0,0 @@
-public class CelsiusToFahrenheit {
-    public static void main(String[] args) {
-        double celsius = 25;
-        double fahrenheit = (celsius * 9 / 5) + 32;
-        System.out.println(celsius + "°C = " + fahrenheit + "°F");
-    }
-}
--- Programming_Element_Practice/PowerCalculation.java
+++
@@ -1,8 +0,0 @@
-public class PowerCalculation {
-    public static void main(String[] args) {
-        int base = 2;
-        int exponent = 5;
-        int result = (int) Math.pow(base, exponent);
-        System.out.println(base + " raised to the power of " + exponent + " = " + result);
-    }
-}
