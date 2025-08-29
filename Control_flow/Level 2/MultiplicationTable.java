67544d7a8e7cbf5c98fefc86334ec63e283fd7d4
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:27:31 +0530

    Delete Control_flow directory

--- Control_flow/Level 2/MultiplicationTable.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-class MultiplicationTable {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 6; i <= 9; i++) System.out.println(n + " * " + i + " = " + (n * i));
-        sc.close();
-    }
-}
