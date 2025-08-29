0ce684733e9582f653a113f3759ab54eb13fe0ac
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:30:00 +0530

    Delete KilometersToMiles.java

--- KilometersToMiles.java
+++
@@ -1,7 +0,0 @@
-public class KilometersToMiles {
-    public static void main(String[] args) {
-        double km = 10;
-        double m = km * 0.621371;
-        System.out.println(km + " km = " + m + " miles");
-    }
-}
