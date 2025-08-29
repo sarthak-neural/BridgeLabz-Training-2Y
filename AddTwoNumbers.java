0f870d91ea5db355a6680fb6a89055892ca1acab
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:24:14 +0530

    Delete AddTwoNumbers.java

--- AddTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        int n = 5;
-        int m = 7;
-        int s = m + n;
-        System.out.println("Sum = " + s);
-    }
-}
