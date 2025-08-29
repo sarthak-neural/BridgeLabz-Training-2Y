a5a1715e45767a35502240161a225b093fb34bc2
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:30:12 +0530

    Delete PerimeterOfRectangle.java

--- PerimeterOfRectangle.java
+++
@@ -1,8 +0,0 @@
-public class PerimeterOfRectangle {
-    public static void main(String[] args) {
-        double l = 8;
-        double w = 5;
-        double p = 2 * (l + w);
-        System.out.println("Perimeter of rectangle = " + p);
-    }
-}
