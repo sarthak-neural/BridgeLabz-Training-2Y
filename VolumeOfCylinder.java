adce021250551bac625765afd114bdfae302725f
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:30:53 +0530

    Delete VolumeOfCylinder.java

--- VolumeOfCylinder.java
+++
@@ -1,8 +0,0 @@
-public class VolumeOfCylinder {
-    public static void main(String[] args) {
-        double radius = 3;
-        double height = 10;
-        double volume = Math.PI * radius * radius * height;
-        System.out.println("Volume of cylinder = " + volume);
-    }
-}
