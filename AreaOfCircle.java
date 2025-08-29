beadc3f7bd731c33bf38f7fa2c862453f49ccaca
Sarthak Agarwal <sarthakagarwal027@gmail.com>
2025-08-29 10:28:54 +0530

    Delete AreaOfCircle.java

--- AreaOfCircle.java
+++
@@ -1,7 +0,0 @@
-public class AreaOfCircle {
-    public static void main(String[] args) {
-        double radius = 5;
-        double area = Math.PI * radius * radius;
-        System.out.println("Area of circle = " + area);
-    }
-}
